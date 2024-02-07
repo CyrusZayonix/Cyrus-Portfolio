c2d2ffab693ba3a742449389dc3346e3c79eb73b
CyrusZayonix <134689407+CyrusZayonix@users.noreply.github.com>
2024-02-07 11:51:40 +0100

    sport aangepast en nieuwe foto's bij toegevoegd

--- sport.php
+++ sport.php
@@ -20,7 +20,24 @@
     <section>
         <h2>Sports</h2>
         <p>On this page you are going to learn about my favorite sports and my All Time Personal records (Pr's)</p>
+        <h2>Here are my all time PR's</h2>
+        <p>Squats 175kg 1x</p>
+        <p>Squats 160kg 6x</p>
+        <p>incline dumbbell bench press 36kg(each) 7x</p>
+        <p>Leg extentions 112kg 33x</p>
+        <p>Dumbbell shoulder press 30kg 7x</p>
+        <p>Lateral raises 20kg(each) 10x</p>
+        <p>close grip T bar rows 100 kg 7x</p>
+        <p>Barbell rows 120kg 8x</p>
+
+
+        <img src="images/squat.jpg" width="250" height="300">
+        <img src="images/gym.jpg" width="300" height="300">
+        <img src="images/cj.jpg" width="300" height="300">
     </section>
+    <section>
+
+    </setion>    
     <footer>
         <p>&copy; <?php echo date("Y"); ?>  Cyrus . All rights reserved.</p>
     </footer>
