ff123a222c60a50facb8857ca594fd99b0397ecd
CyrusZayonix <134689407+CyrusZayonix@users.noreply.github.com>
2024-02-13 10:44:51 +0100

    Home country and CV page geupdated
    Wat meer texten zijn aan home country web page toegevoegd. CV CSS error fixed door Nemo(what a legend he is)

--- CV.php
+++ CV.php
@@ -3,7 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/style.css"> 
+
     <title>Your Portfolio - Home</title>
 </head>
 <body>
@@ -20,7 +21,7 @@
         </ul>
     </nav>
     <section>
-        <button onclick="location.href='Files/CV-C.Moradi.pdf'" download="CV-C.Moradi.pdf">Download CV PDF</button>
+        <button onclick="location.href='Files/CV-C.Moradi.pdf'" download="CV-C.Moradi.pdf">Open CV PDF</button>
     </section>
     <footer>
         <p>&copy; <?php echo date("Y"); ?> Cyrus . All rights reserved.</p>
